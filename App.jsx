3b6f8656dab261fac3ee1b338d2e9acf5848a759
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-07-31 15:58:49 +0530

    Added Navigation Drawer, Updated Home, Added Action Button

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './app/screens/Home/Home';
+import Main from './app/screens/Main/Main';
 import Record from './app/screens/Record/Record';
 import LocationRecordTest from './app/tests/LocationRecordTest/LocationRecordTest';
 
@@ -12,11 +12,12 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator headerMode="none" initialRouteName="LocationRecordTest">
-                <Stack.Screen name="Home" component={Home} />
+            <Stack.Navigator headerMode="none" initialRouteName="Main">
+                <Stack.Screen name="Main" component={Main} />
                 <Stack.Screen name="Record" component={Record} />
                 <Stack.Screen name="LocationRecordTest" component={LocationRecordTest} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
+
