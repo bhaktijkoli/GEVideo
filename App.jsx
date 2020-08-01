a3c211ffd1e4e4d622c63549a43ada53414a65e1
Bhupender Singh <bhpndrsingh306@gmail.com>
2020-08-01 08:16:03 +0530

    Video player, Control components, Play/Pause

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Main from './app/screens/Main/Main';
 import Record from './app/screens/Record/Record';
+import Play from './app/screens/Play/Play';
 import LocationRecordTest from './app/tests/LocationRecordTest/LocationRecordTest';
 
 const Stack = createStackNavigator();
@@ -12,12 +13,12 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator headerMode="none" initialRouteName="Main">
+            <Stack.Navigator headerMode="none" initialRouteName="Play">
                 <Stack.Screen name="Main" component={Main} />
                 <Stack.Screen name="Record" component={Record} />
+                <Stack.Screen name="Play" component={Play} />
                 <Stack.Screen name="LocationRecordTest" component={LocationRecordTest} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
-
